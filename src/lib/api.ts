d0e104b460d6b70ec3a378b2c2fda836ee1543e5
Suresh <98641509+suresh-ben@users.noreply.github.com>
2025-06-27 02:06:17 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3001/api",
+    baseURL: "https://poc-totp.vercel.app/api",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
